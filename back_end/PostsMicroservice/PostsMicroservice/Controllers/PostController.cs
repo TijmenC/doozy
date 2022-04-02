d86ae62da9f956496faedfe35ea9fe596c91d35d
Coenders,Tijmen T.C.P <70951009+TijmenC@users.noreply.github.com>
2022-04-02 19:18:58 +0200

    Added Post Controllers and models

---
+++ back_end/PostsMicroservice/PostsMicroservice/Controllers/PostController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostsMicroservice.DTO;
+using PostsMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostsMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly PostContext _context;
+
+        public QuestionController(PostContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Question/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PostDTO>> GetQuestion(int id)
+        {
+            var question = await _context.Posts.FindAsync(id);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return QuestionToDTO(question);
+        }
+
+        private static PostDTO QuestionToDTO(Post todoItem) =>
+      new PostDTO
+      {
+          Id = todoItem.Id,
+          Title = todoItem.Title,
+          Description = todoItem.Description
+      };
+    }
+  }
--- back_end/PostsMicroservice/PostsMicroservice/Models/PostContext.cs
+++ back_end/PostsMicroservice/PostsMicroservice/Models/PostContext.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace PostsMicroservice.Models
 {
-    public class PostContext
+    public class PostContext : DbContext
     {
+        public PostContext(DbContextOptions<PostContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Post>().ToTable("Posts");
+        }
     }
 }
