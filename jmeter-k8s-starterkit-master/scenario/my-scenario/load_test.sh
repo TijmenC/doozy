753394c0f1a040ebabb89d34e704f4e1532f6c92
Tijmen <70951009+TijmenC@users.noreply.github.com>
2022-05-26 21:06:43 +0200

    More updated IP Adresses

--- jmeter-k8s-starterkit-master/scenario/my-scenario/load_test.sh
+++ jmeter-k8s-starterkit-master/scenario/my-scenario/load_test.sh
@@ -1,8 +1,8 @@
-slave_array=(10.1.1.63); index=1 && while [ ${index} -gt 0 ]; do for slave in ${slave_array[@]}; do if echo 'test open port' 2>/dev/null > /dev/tcp/${slave}/1099; then echo ${slave}' ready' && slave_array=(${slave_array[@]/${slave}/}); index=$((index-1)); else echo ${slave}' not ready'; fi; done; echo 'Waiting for slave readiness'; sleep 2; done
+slave_array=(10.1.0.214); index=1 && while [ ${index} -gt 0 ]; do for slave in ${slave_array[@]}; do if echo 'test open port' 2>/dev/null > /dev/tcp/${slave}/1099; then echo ${slave}' ready' && slave_array=(${slave_array[@]/${slave}/}); index=$((index-1)); else echo ${slave}' not ready'; fi; done; echo 'Waiting for slave readiness'; sleep 2; done
 echo "Installing needed plugins for master"
 cd /opt/jmeter/apache-jmeter/bin
 sh PluginsManagerCMD.sh install-for-jmx my-scenario.jmx
-jmeter -Ghost=doozy-posts -Gport=80 -Gprotocol=http -Gthreads=10 -Gduration=60 -Grampup=6  --logfile /report/my-scenario.jmx_2022-05-26_164320.jtl --nongui --testfile my-scenario.jmx -Dserver.rmi.ssl.disable=true --remoteexit --remotestart 10.1.1.63 >> jmeter-master.out 2>> jmeter-master.err &
+jmeter -Ghost=10.1.0.127 -Gport= -Gprotocol=http -Gthreads=10 -Gduration=60 -Grampup=6 --reportatendofloadtests --reportoutputfolder /report/report-my-scenario.jmx-2022-05-26_210234 --logfile /report/my-scenario.jmx_2022-05-26_210234.jtl --nongui --testfile my-scenario.jmx -Dserver.rmi.ssl.disable=true --remoteexit --remotestart 10.1.0.214 >> jmeter-master.out 2>> jmeter-master.err &
 trap 'kill -10 1' EXIT INT TERM
 java -jar /opt/jmeter/apache-jmeter/lib/jolokia-java-agent.jar start JMeter >> jmeter-master.out 2>> jmeter-master.err
 wait
