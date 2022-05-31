9651683e15137a468b265278b5a6b8d215533203
Coenders,Tijmen T.C.P <70951009+TijmenC@users.noreply.github.com>
2022-05-31 15:31:17 +0200

    Added second migration

---
+++ back_end/ProfileMicroservice/ProfileMicroservice/Migrations/20220531133052_Second.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProfileMicroservice.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionTime",
+                table: "Users",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletionTime",
+                table: "Users");
+        }
+    }
+}
