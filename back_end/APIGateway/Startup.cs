951fb12d159daa10cf72c67973c9cc0b7413c4fa
Coenders,Tijmen T.C.P <70951009+TijmenC@users.noreply.github.com>
2022-06-03 12:04:03 +0200

    Added needed files

--- back_end/APIGateway/Startup.cs
+++ back_end/APIGateway/Startup.cs
@@ -9,17 +9,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Identity.Web;
+using Microsoft.Extensions.Configuration;
 
 namespace APIGateway
 {
     public class Startup
     {
+        public IConfiguration Configuration { get;  }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
             services.AddOcelot();
+            services.AddMicrosoftIdentityWebApiAuthentication(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +41,8 @@ namespace APIGateway
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseCors("AllowSpecificOrigin");
 
